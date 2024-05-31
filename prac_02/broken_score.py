ac18e16552bad36328b10ea2dabb9fbd658389b7
Luke-hawkins <145887513+Luke-hawkins@users.noreply.github.com>
2024-05-31 21:05:24 +1000

    Added a main and get_result function to program.

--- prac_02/broken_score.py
+++ prac_02/broken_score.py
@@ -4,13 +4,23 @@ Broken program to determine score status
 """
 
 
-score = float(input("Enter score: "))
-while score > 100 or score < 0:
-    print("Invalid score")
+def main():
+    """Generates a score bases on results"""
     score = float(input("Enter score: "))
-if score >= 90:
-    print("Excellent")
-elif score >= 50:
-    print("Passable")
-else:
-    print("Bad :(")
+    while score > 100 or score < 0:
+        print("Invalid score")
+        score = float(input("Enter score: "))
+    result = get_result(score)
+    print(result)
+
+def get_result(score):
+    """Return results based on score"""
+    if score >= 90:
+        return "Excellent"
+    elif score >= 50:
+        return "Passable"
+    else:
+        return "Bad :("
+
+
+main()
