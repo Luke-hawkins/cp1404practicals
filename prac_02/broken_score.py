ff9567208ca9e01428428bf8e356fe57cf324de7
Luke-hawkins <145887513+Luke-hawkins@users.noreply.github.com>
2024-05-31 21:22:31 +1000

    Added random score generator

--- prac_02/broken_score.py
+++ prac_02/broken_score.py
@@ -2,7 +2,7 @@
 CP1404/CP5632 - Practical
 Broken program to determine score status
 """
-
+import random
 
 def main():
     """Generates a score bases on results"""
@@ -12,6 +12,10 @@ def main():
         score = float(input("Enter score: "))
     result = get_result(score)
     print(result)
+    score = random.randint(0,100)
+    result = get_result(score)
+    print(f"Random result is {result}")
+
 
 def get_result(score):
     """Return results based on score"""
