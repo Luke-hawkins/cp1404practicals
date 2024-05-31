6f94ce3b639925bd2eb28464579201eef9892cb5
Luke-hawkins <145887513+Luke-hawkins@users.noreply.github.com>
2024-05-31 21:51:26 +1000

    added menu function to score_menu and added get_valid_score & get_result function

---
+++ prac_02/score_menu.py
@@ -0,0 +1,31 @@
+def main():
+    """Allow user to input score and output results based on the score"""
+    MENU = "(G)et a valid score (must be 0-100 inclusive) \n(P)rint result \n(S)how stars ) \n(Q)uit"
+    score = get_valid_score()
+    print(MENU)
+    choice = input(">>> ").upper()
+    while choice != "Q":
+        if choice == "G":
+            score = get_valid_score()
+        elif choice == "P":
+            result = get_result(score)
+            print(result)
+def get_valid_score():
+    """get a valid score from 0 to 100"""
+    score = float(input("Enter score: "))
+    while score > 100 or score < 0:
+        print("Invalid score")
+        score = float(input("Enter score: "))
+    return score
+
+def get_result(score):
+    """Return results based on score"""
+    if score >= 90:
+        return "Excellent"
+    elif score >= 50:
+        return "Passable"
+    else:
+        return "Bad :("
+
+
+main()
