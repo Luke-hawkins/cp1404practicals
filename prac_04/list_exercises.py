caa17481920e908636b492a923402cbcbe90c5f3
Luke-hawkins <145887513+Luke-hawkins@users.noreply.github.com>
2024-06-16 21:58:42 +1000

    add function to display summary of numbers in numbers_list

---
+++ prac_04/list_exercises.py
@@ -0,0 +1,21 @@
+def main():
+    """Gets list of numbers from user"""
+    TOTAL_INPUT_NUMBERS = 5
+    numbers = get_numbers(TOTAL_INPUT_NUMBERS)
+    display_numbers_summary(numbers)
+
+def get_numbers(TOTAL_INPUT_NUMBERS):
+    """Gets numbers from uses depending on TOTAL_INPUT_NUMBERS"""
+    numbers = []
+    while len(numbers) != TOTAL_INPUT_NUMBERS:
+        numbers.append(int(input("Number: ")))
+    return numbers
+
+def display_numbers_summary(numbers):
+    """display overview of numbers in numbers list"""
+    sorted_numbers = sorted(numbers)
+    print(f"The first number is {numbers[0]} \nThe last number is {numbers[-1]} \n"
+          f"The smallest number is {sorted_numbers[0]} \nThe largest number is {sorted_numbers[-1]}\n"
+          f"The average of the numbers is {sum(numbers) / len(numbers)}")
+
+main()
