b833c2a8143a35feac11f661b33cc067fd04d88f
Luke-hawkins <145887513+Luke-hawkins@users.noreply.github.com>
2024-06-16 14:26:00 +1000

    finish list_warmup

---
+++ prac_04/lists_warmup.py
@@ -0,0 +1,17 @@
+numbers = [3, 1, 4, 1, 5, 9, 2]
+
+# Q1. 3
+# Q2. 2
+# Q3. 1
+# Q4. [3, 1, 4, 1, 5, 9]
+# Q5. 1
+# Q6. True
+# Q7. False
+# Q8. False
+# Q9.[3, 1, 4, 1, 5, 9, 2, 6, 5, 3]
+
+
+numbers[0] = "ten"
+numbers[-1] = 1
+print(numbers[2:])
+print(9 in numbers)
